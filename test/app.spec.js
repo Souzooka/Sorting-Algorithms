f01a2efc5c43e48b0d3cc88bb896ca17f171ec88
Dakota <bazookaharwell@gmail.com>
2017-04-04 13:47:40 -1000

    bubble sort test

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,3 +1,12 @@
 var chai = require('chai');
 var expect = chai.expect;
 var sortingModule = require('../app.js')();
+
+describe('Bubble sort', () => {
+  var bubbleSort = sortingModule.bubbleSort;
+
+  it('should sort an array', () => {
+    expect(bubbleSort([3, 2, 1])).to.be.deep.equal([1, 2, 3]);
+  });
+
+});
